2d11ca0411898bda079769449141ce9054c94593
Aathavan Sivakumaran <aathavan@Aathavans-MacBook-Pro.local>
2025-02-27 22:08:07 +0000

    Fix image component usage

--- src/app/productpage/product.page.tsx
+++ src/app/productpage/product.page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { mainProducts } from '../data/data';
 import PriceDisplay from '../pricedisplay/pricedisplay';
 
@@ -6,10 +7,15 @@ const ProductPage: React.FC = () => {
   return (
     <div>
       <h1>{mainProducts[0].name}</h1>
-      <img 
-        src={mainProducts[0].images[0].url} 
-        alt={mainProducts[0].images[0].altText} 
-      />
+          <Image
+            src={image.url}
+            alt={image.altText}
+            className={styles.productImage}
+            width={500}
+            height={500}
+            priority
+          />
+
       <PriceDisplay product={mainProducts[0]} />
     </div>
   );
