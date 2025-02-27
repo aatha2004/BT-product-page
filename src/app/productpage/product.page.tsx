4e85a800ce3b810260d6d5d3f7e0cecba4274e65
Aathavan Sivakumaran <aathavan@Aathavans-MacBook-Pro.local>
2025-02-27 22:13:52 +0000

    Update next.config for image patterns

--- src/app/productpage/product.page.tsx
+++ src/app/productpage/product.page.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import { mainProducts } from '../data/data';
 import PriceDisplay from '../pricedisplay/pricedisplay';
+import styles from './productpage.module.css';
 
 const ProductPage: React.FC = () => {
   return (
     <div>
       <h1>{mainProducts[0].name}</h1>
+      {mainProducts[0].images.map((image, index) => (
           <Image
+            key={index}
             src={image.url}
             alt={image.altText}
             className={styles.productImage}
@@ -15,6 +18,7 @@ const ProductPage: React.FC = () => {
             height={500}
             priority
           />
+      ))}
 
       <PriceDisplay product={mainProducts[0]} />
     </div>
